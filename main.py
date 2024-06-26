30238fdbb744c9fbc7e3ffb045306570c70a6a6c
MaurquiseBradford <bmaurquise04@gmail.com>
2024-06-26 15:31:09 -0500

    Reading Data from Data Tables from MySQL in main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from mysql.connector import Error
 import create_database_query as cdq
 import create_data_table_queries as cdtq
 import populate_data_tables_queries as pdtq
+import read_data_tables_queries as rdtq
 
 # create function to establish connection from main.py to MySQL
 def create_server_connection(host_name, user_name, user_password, db_name):
@@ -41,11 +42,46 @@ def execute_query(connection, query):
         print(f"Error: '{err}'")
 
 
+# create a function to read information from data tables from DB
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+
 # call function to establish connection from main.py to MySQL
 connection = create_server_connection("localhost", "root","student", "school")
 
-# call work horse function.
-execute_query(connection, pdtq.populate_course_table)
-execute_query(connection,pdtq.populate_teacher_table)
-execute_query(connection,pdtq.populate_participant_table)
-execute_query(connection, pdtq.populate_client_table)
+def displayAllDataTables():
+    # display information from data tables in pycharm
+    print("Information for Client Data Table: ")
+    clientDataTable = read_query(connection, rdtq.display_client_table_information)
+    for clientInformation in clientDataTable:
+        print(clientInformation)
+    print()
+
+    print("Information for Teacher Table: ")
+    teacherDataTable = read_query(connection, rdtq.display_teacher_table_information)
+    for teacherInformation in teacherDataTable:
+        print(teacherInformation)
+    print()
+
+    print("Information for Participant Table")
+    participantDataTable = read_query(connection, rdtq.display_participant_table_information)
+    for participantInformation in participantDataTable:
+        print(participantInformation)
+    print()
+
+    print("Information for Course Table: ")
+    courseDataTable = read_query(connection, rdtq.display_course_table_information)
+    for courseInformation in courseDataTable:
+        print(courseInformation)
+    print()
+
+displayAllDataTables()
---
+++ read_data_tables_queries.py
@@ -0,0 +1,16 @@
+display_teacher_table_information = """
+SELECT * FROM teacher;
+"""
+
+display_client_table_information = """
+SELECT *FROM client;
+"""
+
+display_course_table_information = """
+SELECT * FROM course;
+"""
+
+display_participant_table_information = """
+SELECT * FROM participant;
+"""
+
